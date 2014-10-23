0c8d71c865eff4599b6c38642a40293d8b63a899
fosterz <zacharyfoster1989@gmail.com>
2014-10-23 16:02:34 -0700

    fixed import from error

--- R/graphing.R
+++ R/graphing.R
@@ -2,7 +2,6 @@
 #'
 #' Adds lines in a ggplot2 plot for the maximal absorption of compunds relevent to molecular biology.
 #' @export
-#' @importFrom ggplot2 geom_vline
 nanodrop_absorbtion_maxima <- function() {
   absorption_maxima <- data.frame(Substance=c("DNA", "Protein"), value=c(260, 280))
   geom_vline(data=absorption_maxima,
