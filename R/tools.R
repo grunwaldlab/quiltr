a2f7e52c0a55bdad66a32159d95f083f6ed5ad35
Zachary Foster <zacharyfoster1989@gmail.com>
2015-05-06 22:35:25 -0700

    copy_folder_with_links done

--- R/tools.R
+++ R/tools.R
@@ -8,76 +8,23 @@
 #' @param to (\code{character}) Where to copy the folder to. 
 #' 
 copy_folder_with_links <- function(from, to) {
-  copy_path <- function(from, to) {
-    if (Sys.readlink(from) != "") { #if it is a link
-      file.link(from = to, to = Sys.readlink(from))
-    } else if (file.info(from)$isdir) {
-      dir.create(to, recursive = TRUE)
-    } else {
-      file.copy(from = from, to = to)
-    }
-  }
-  from <- normalizePath(from)
-  to <- normalizePath(to)
   target <- file.path(to, basename(from))
-  if (file.exists(target)) stop(paste0("Directory '", target, "' already exists."))
-  from_paths <- list.files(from, full.names = TRUE, recursive = TRUE, )
-  to_paths <- file.path(to, basename(from), list.dirs(from, full.names = FALSE, recursive = TRUE))
-  invisible(mapply(copy_path, from = from_paths, to = to_paths))
-}
-
-
-
-
-copy_folder_with_links <- function(from, to) {
-#   from <- normalizePath(from)
-#   to <- normalizePath(to)
-  target <- file.path(to, basename(from))
-  if (Sys.readlink(from) != "") { #if it is a link
-    invisible(file.link(from = target, to = Sys.readlink(from)))
-  } else if (file.info(from)$isdir) {
-    invisible(dir.create(target, recursive = TRUE))
-    from_paths <- list.files(from, full.names = TRUE, recursive = FALSE, include.dirs = TRUE)
-    to_paths <- target
-    if (length(from_paths) > 0) {
-      invisible(mapply(copy_folder_with_links, from = from_paths, to = to_paths))
-    }
-  } else {
-    invisible(file.copy(from = from, to = target))
+  if (file.exists(target)) stop(paste0("Target folder ", target, " already exists."))
+  # Get list of all files/folders to copy ----------------------------------------------------------
+  path <- data.frame(target = list.files(from, recursive = TRUE, all.files = TRUE, include.dirs = TRUE))
+  path$from  <- file.path(from, path$target)
+  path$to  <- file.path(to, basename(from), path$target)
+  # Get type of file/folders -----------------------------------------------------------------------
+  path$type <- factor("file", levels = c("file", "folder", "link"))
+  path$type[file.info(path$from)$isdir] <- "folder"
+  path$type[Sys.readlink(path$from) != ""] <- "link"
+  # Remove all files that are descendants of links -------------------------------------------------
+  is_child <- function(query, refs) {
+    sapply(refs, function(x) grepl(paste0("^", x), query) & query != x)
   }
-}
-
-
-
-
-copy_folder_with_links <- function(from, to) {
-  targets <- file.path(to, basename(from))
-  dirs <- targets[file.info(from)$isdir]
-  links <- targets[Sys.readlink(from) != ""]
-  files <- targets[file.info(from)$isdir == FALSE & Sys.readlink(from) == ""]
-  
-  if(length(dirs) > 0) {
-    dir.create(dirs)
-  }
-  if(length(files) > 0) {
-    file.copy(from = from[file.info(from)$isdir], to = files)
-  }
-  if(length(links) > 0) {
-    file.link(from = links, to = Sys.readlink(from)[Sys.readlink(from) != ""])
-  }
-  invisible(mapply(copy_folder_with_links, from = from_paths, to = to_paths))
-  
-  
-  if (Sys.readlink(from) != "") { #if it is a link
-    invisible(file.link(from = target, to = Sys.readlink(from)))
-  } else if (file.info(from)$isdir) {
-    invisible(dir.create(target, recursive = TRUE))
-    from_paths <- list.files(from, full.names = TRUE, recursive = FALSE, include.dirs = TRUE)
-    to_paths <- target
-    if (length(from_paths) > 0) {
-      invisible(mapply(copy_folder_with_links, from = from_paths, to = to_paths))
-    }
-  } else {
-    invisible(file.copy(from = from, to = target))
-  }
+  path <- path[!sapply(path$from, function(x) any(is_child(x, path$from) & path$type == "link")), ]
+  # Make copy --------------------------------------------------------------------------------------
+  invisible(lapply(path$to[path$type == "folder"], dir.create, recursive = TRUE))
+  invisible(file.copy(from = path$from[path$type == "file"], to = path$to[path$type == "file"]))
+  invisible(file.symlink(Sys.readlink(path$from[path$type == "link"]), path$to[path$type == "link"]))
 }
