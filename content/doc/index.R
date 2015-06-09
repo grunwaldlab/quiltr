24c0ce4db7eaa0f0caa3452e616cd7ddb1d6fdba
Zachary Foster <zacharyfoster1989@gmail.com>
2015-06-08 21:11:17 -0700

    Automated commit by quiltr::make_gh_website

--- content/doc/index.R
+++ content/doc/index.R
@@ -1,1 +1,9 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("devtools")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  devtools::install_github("grunwaldlab/quiltr")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library(quiltr)
 
