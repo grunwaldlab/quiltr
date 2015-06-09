b3f85f53d0c46ff01e486341f93541b3d8e68852
Zachary Foster <zacharyfoster1989@gmail.com>
2015-06-09 03:33:33 -0700

    Automated commit by quiltr::make_gh_website

--- content/doc/introduction.R
+++ content/doc/introduction.R
@@ -9,7 +9,10 @@ file.copy(from = "placement.yml", to = doc_path, overwrite = TRUE)
 
 ## ----make_test_website_1, echo=FALSE, results='hide', eval=FALSE---------
 #  # This is not run automatically due to rmarkdown issue #248, but is used to make the website example
-#  quiltr::make_website(path = "vignette_examples/test_website_1", output = "website_source", overwrite = TRUE, output_dir_name = "test_website_1", theme = "cerulean")
+#  quiltr::quilt(path = "vignette_examples/test_website_1", output = "website_source", overwrite = TRUE, output_dir_name = "test_website_1", theme = "cerulean")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  quilt(path = "/home/your_name/documents/notes")
 
 ## ---- echo=FALSE, comment=""---------------------------------------------
 cat(readChar("../vignette_examples/test_website_2/old/note_config.yml", nchars = 10000))
@@ -19,5 +22,8 @@ cat(readChar("../vignette_examples/test_website_2/old/sub_dir/note_config.yml", 
 
 ## ----make_test_website_2, echo=FALSE, results='hide', eval=FALSE---------
 #  # This is not run automatically due to rmarkdown issue #248, but is used to make the website example
-#  quiltr::make_website(path = "vignette_examples/test_website_2", output = "website_source", overwrite = TRUE, output_dir_name = "test_website_2", note_config_name = "note_config.yml", theme = "cerulean")
+#  quiltr::quilt(path = "vignette_examples/test_website_2", output = "website_source", overwrite = TRUE, output_dir_name = "test_website_2", note_config_name = "note_config.yml", theme = "cerulean")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  quilt(path = "/home/your_name/documents/notes", note_config_name = "note_config.yml")
 
