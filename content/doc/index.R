5b766520f63559edf48b73a8c6f60d02d2b383ab
fosterz <zacharyfoster1989@gmail.com>
2015-06-12 14:07:24 -0700

    Automated commit by quiltr::make_gh_website

--- content/doc/index.R
+++
@@ -1,9 +0,0 @@
-## ---- eval=FALSE---------------------------------------------------------
-#  install.packages("devtools")
-
-## ---- eval=FALSE---------------------------------------------------------
-#  devtools::install_github("grunwaldlab/quiltr")
-
-## ---- eval=FALSE---------------------------------------------------------
-#  library(quiltr)
-
