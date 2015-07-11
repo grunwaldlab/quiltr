4c808eacd8f1369275bec725b49e2b3159a0a1a8
fosterz <zacharyfoster1989@gmail.com>
2015-07-10 17:27:16 -0700

    starting to become literate

--- R/get_options.R
+++ R/get_options.R
@@ -1,3 +1,95 @@
+#| ## Option Parsing
+#|
+#| Perhaps the most important goal of `quiltr` is to place the fewest restrictions possible on input folder structure. 
+#| This can be partly accomplished by having numerous options to change how folders are interpreted, such as an option to ignore file names.
+#| However, any one set of option values might not be optimal for all parts of a complex folder.
+#| Such "global" options only allow users to choose their restrictions rather than allowing true freedom of folder structure.
+#| Users should ask "how can I configure quiltr to represent this folder?" rather than "how can I make this folder usable with quiltr?".
+#|
+#| ### Path-specific options
+#| 
+#| To maximize the flexibility of `quiltr`, most option values should be file-path-specific.
+#| Some options will only apply to files, such as an option specifying which file types to include, whereas others will only apply to folders, such as an option specifying where to look for files.
+#| If a folder has been organized well, such file-path-specific options should be intiutive to apply.
+#| For example, a folder might contain python scripts that the user wants to display, so an option like the following might be used:
+#| 
+#| ```{r, eval = FALSE}
+#| display = c("py")
+#| ```
+#|
+#| However, an other part of the folder, lets say `src/common_lib`, might contain a standard library of python code that the script uses, but the user does not want to display. 
+#| Therefore, there needs to be a way to specify which parts of the folder use which options. 
+#| One way this could be done is to make each option accpet a list of named values, with names correspoonsing to file paths.
+#| For example, the option might specified by:
+#|
+#| ```{r, eval = FALSE}
+#| display = list('**' = c('py'), 'src/**' = c())
+#| ```
+#|
+#| The example above uses [recursive filename expansion](https://en.wikipedia.org/wiki/Glob_%28programming%29#Recursive_Globbing) (a.k.a. globbing) to specify that all python files will be displayed except those in `src`.
+#| Note that order of option values matters; the more specific value for `src/**` overwrites the more general value for `**`.
+#| If the order was reversed,  `src/**` would have no effect. 
+#| The options path could also be specified using [regular expressions](https://en.wikipedia.org/wiki/Regular_expression) instead of filename expansion.
+#| In that case, the above example would be: 
+#|
+#| ```{r, eval = FALSE}
+#| display = list('.*' = c('py'), 'src/.*' = c())
+#| ```
+#| 
+#| Although this syntax increases flexibility, it can easily result in excessivly long commands.
+#| This can be ameliorated in two ways:
+#|
+#| * logical defaults for paths if they are not specified
+#| * configuration files containing option values for each folder. 
+#|
+#| The following sections will deal with these two ideas
+#|
+#|
+#| ### Default paths for path-specific options
+#|
+#| If a path for an option value is not specified, the most logical default would be to match all paths under the current folder (i.e. `**`).
+#| When non-absolute file paths are used, this brings up the question of what the "current folder" is. 
+#| In this case, it would be the value of the `path` option given to the `quilt` function, but this will become more complex and important when we consider configuration files.
+#| If we assume that the default should match all files under the current path, a simplified version of the above example could be:
+#|
+#| ```{r, eval = FALSE}
+#| display = list(c('py'), 'src/.*' = c())
+#| ```
+#|
+#| If we assume further that all option values should be coerced into lists, the first example in this section: 
+#|
+#| ```{r, eval = FALSE}
+#| display = c("py")
+#| ```
+#|
+#| would be interpreted by as:
+#|
+#| ```{r, eval = FALSE}
+#| display = list('**' = c('py'))
+#| ```
+#|
+#| The intuitive usage that we started with is now a special case of the more flexible usage. 
+#| This behavior is desireable because it allows novice users to use simple commands while still allowing for more advanced usage.
+#| 
+#| ### Configuration files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #===================================================================================================
 #' Get paths to configuration files 
 #' 
