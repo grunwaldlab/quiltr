9db2ac5684644d4e86be47c50d4b00c0d603e6f1
Zachary Foster <zacharyfoster1989@gmail.com>
2015-07-30 09:26:16 -0700

    new

---
+++ tests/testthat/chapter_02--tests--parse_configuration_files.R
@@ -0,0 +1,10 @@
+#+ echo = FALSE
+knitr::opts_chunk$set(eval = FALSE)
+#|
+#| # Tests for parsing configuration files 
+#|
+
+
+library(quiltr)
+library(testthat)
+context("Getting path-specific option values")
