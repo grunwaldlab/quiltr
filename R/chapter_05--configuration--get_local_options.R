c00069e7ee7a6dc926932591effd439db05750f6
Zachary Foster <zacharyfoster1989@gmail.com>
2015-12-08 18:04:52 -0800

    added roxygen draft

--- R/chapter_05--configuration--get_local_options.R
+++ R/chapter_05--configuration--get_local_options.R
@@ -3,9 +3,32 @@ knitr::opts_chunk$set(eval = FALSE)
 #|
 #| # Getting local options
 #|
-#|
-
-
+#| 
+#' @title 
+#' Get local options from configuration files
+#' 
+#' @description 
+#' Dertermines the options values of a renderer function for a set of file paths by parsing any 
+#' configuration files that might be present in a set of folders.
+#' Default option values defined in the renderer function are used if no configuration file settings apply. 
+#' 
+#' @param sub_function (\code{function} of length 1)
+#' The function whose options are set using configuration files.
+#' 
+#' @param target_paths (\code{character})
+#' Files paths that can each have a unique set of options values each.
+#' These will correspond to one dimension of the output matrix.
+#' 
+#' @param config_paths (\code{character})
+#' Folder paths where configuration files might exist.
+#' 
+#' @param global_options (\code{character})
+#' 
+#' 
+#' @return a 2-dimentional \code{list}
+#' 
+#' Given a set of file paths and folder paths that might contain configuration files, this function
+#' returns the local options that apply to 
 get_path_specific_options <- function(sub_function, target_paths, config_paths, global_options) {
   main_function <- "quilt"
   
@@ -22,7 +45,9 @@ get_path_specific_options <- function(sub_function, target_paths, config_paths, 
   #| ### Order `config_paths` by folder depth
   #| Options defined in parent directories should be overwritten by options defined in their children. 
   #| Therefore, configurations files in folders closer to root should be applied first.
-  
+  path_depth <- vapply(strsplit(config_paths, split = .Platform$file.sep, fixed = TRUE),
+                       FUN = length, FUN.VALUE = numeric(1))
+  config_paths <- config_paths[order(path_depth)]
   
   #| ### Parse configuration files
   #| All of the configuration files are parsed at the same time and consolidated into the same 2-dimentional list.
@@ -36,4 +61,6 @@ get_path_specific_options <- function(sub_function, target_paths, config_paths, 
     # Apply the setting to the relevant files
     
   }
+  
+  #| Return the output data
 }
